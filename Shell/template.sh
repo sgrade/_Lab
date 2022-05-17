69117d5c9fb352895b7d49b1102f384af0634e36
Roman K <sgrade@users.noreply.github.com>
2022-05-17 10:32:09 +0200

    shell

---
+++ Shell/template.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# All files (not directories) in a directory
+files=$(find . -maxdepth 1 -type f)
+# echo $files
+
+# file names
+for f in $files; do
+	echo $f | cut -c3-
+done
+
+# grep "source" in files
+for f in $files; do
+  grep source $f
+done
+
+# multiline comment below
+<<comment
+if [ -e "$FILE" ]; then
+	if [ -f "$FILE" ]; then
+		echo "$FILE is a regular file."
+	fi
+	if [ -d "$FILE" ]; then
+		echo "$FILE is a directory."
+	fi
+	if [ -r "$FILE" ]; then
+		echo "$FILE is readable."
+	fi
+	if [ -w "$FILE" ]; then
+		echo "$FILE is writable."
+	fi
+	if [ -x "$FILE" ]; then
+		echo "$FILE is executable/searchable."
+	fi
+else
+	echo "$FILE does not exist"
+	exit 1
+fi
+comment
