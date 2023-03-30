ecfd1e80d59cdbcd142d5aeeec108c1c7bab2000
sgrade <sgrade@users.noreply.github.com>
2023-03-30 17:23:33 +0000

    leetcode

---
+++ Shell/leetcode/tenth-line.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# 195. Tenth Line
+# https://leetcode.com/problems/tenth-line/
+sed -n 10p tenth-line.txt
--- Shell/leetcode/valid-phone-numbers.sh
+++ Shell/leetcode/valid-phone-numbers.sh
@@ -3,4 +3,4 @@
 # 193. Valid Phone Numbers
 # https://leetcode.com/problems/valid-phone-numbers/
 
-grep -E '^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$|^[0-9]{3}-[0-9]{3}-[0-9]{4}$' file.txt
+grep -E '^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$|^[0-9]{3}-[0-9]{3}-[0-9]{4}$' valid-phone-numbers.txt
