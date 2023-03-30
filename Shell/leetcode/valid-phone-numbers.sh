307eb06c4bd740830203122aa9f981b19a986fd1
sgrade <sgrade@users.noreply.github.com>
2023-03-30 16:45:03 +0000

    leetcode

---
+++ Shell/leetcode/valid-phone-numbers.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# 193. Valid Phone Numbers
+# https://leetcode.com/problems/valid-phone-numbers/
+
+grep -E '^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$|^[0-9]{3}-[0-9]{3}-[0-9]{4}$' file.txt
